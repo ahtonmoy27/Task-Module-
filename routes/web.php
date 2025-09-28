16a35cfa5538675ee538a849c5dabbce48f1eec2
Tonmoy <hasnatiucse@gmail.com>
2025-09-28 12:11:16 +0600

    project Done

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TaskController;
 
-Route::get('', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
